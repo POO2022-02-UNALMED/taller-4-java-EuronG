7ae8534ee5c355606249510fa53fa1d40c06675b
EuronG <jugiraldoja@unal.edu.co>
2022-09-02 15:11:33 -0500

    2.0

--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -5,26 +5,27 @@ public class Grupo {
     Persona[] estudiantes;
     Persona profesor;
     Asignatura asignatura;
-    final int codigo = 0;
+    final int codigo;
     String horario;
 
-    Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
+    Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigoo, String horario) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
-        this.codigo = codigo;
+        codigo = codigoo;
         this.horario = horario;
     }
 
     Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        Persona[] personas = new Persona[cantidadEstudiantes];
-        this(personas, profesor, asignatura, codigo, horario);
+       // Persona[] personas = new Persona[cantidadEstudiantes];
+        this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
     }
 
     Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
         this.estudiantes = estudiantes;
         this.profesor = profesor;
         this.asignatura = asignatura;
+        codigo = 0;
     }
 
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
